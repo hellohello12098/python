2d54f51fcaf82b4d2a9f85072e318024bbd5fd4e
hellohello12098 <76791299+hellohello12098@users.noreply.github.com>
2022-07-23 13:57:09 +0100

    Add files via upload

---
+++ net price.py
@@ -0,0 +1,13 @@
+# Take price and display net price after 10% discount
+
+# input
+price = int(input("Enter price :"))
+
+#process
+discount = price * 10 // 100
+#tax
+tax = price * 8 // 100
+net_price = price + tax - discount
+
+# output
+print("Net price =", net_price)
